73fd38bc6882460ab1d59fbc8f400f4461b36039
Joao Dev <joaodevelop@gmail.com>
2017-06-09 16:17:04 -0300

    challenge-32

--- challenge-29.js
+++ challenge-29.js
@@ -1,131 +1,158 @@
 (function($, doc) {
   'use strict';
 
-  /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
-
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
-
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
-
-  Essas informações devem ser adicionadas no HTML via Ajax.
-
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
-
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
-
   var app = (function app(){
 
     var $companyName = $('[data-js="companyName"]');
     var $companyPhone = $('[data-js="companyPhone"]');
-    var $btnCadastrar = $('[data-js="btnCadastrar"]');
-    var $carsTable = $('[data-js="carsTable"]');
+    var $btnRegister = $('[data-js="btnRegister"]');
+    var $tableCars = $('[data-js="carsTable"]');
     var $inputsForm = $('[data-js="ipt"]');
-    var docFragment = doc.createDocumentFragment();
-    var ajax = new XMLHttpRequest();
+    var htmlFragment = doc.createDocumentFragment();
+    var ajaxCompanyInfo = new XMLHttpRequest();
+    var ajaxGetCars = new XMLHttpRequest();
+    var ajaxPostCar = new XMLHttpRequest();
 
-    var registerNewCar = function registerNewCar(event){
-      event.preventDefault();
-      if(allFieldsFilled())
-        newTableLine();
-      else
-        alert('Por favor, preencha todos os campos.');
-    };
+    function getCompanyInfo(){
+      if(isAjaxReady(ajaxCompanyInfo)){
+        $companyName.textContent = JSON.parse(ajaxCompanyInfo.responseText).name;
+        $companyPhone.textContent = JSON.parse(ajaxCompanyInfo.responseText).phone;
+      }
+    }
 
-    var newTableLine = function newTableLine(){
-      var row = doc.createElement('TR');
-      $inputsForm.forEach(function fillRowCells(item){
-        createDataCell(row, item)
+    function getCars(){
+      if(isAjaxReady(ajaxGetCars)){
+        JSON.parse(ajaxGetCars.responseText).forEach(function(car){
+          htmlFragment.appendChild(putCarInRow(car));
+        });
+        $tableCars.appendChild(htmlFragment);
+      }
+    }
+
+    function putCarInRow(car){
+      var tr = doc.createElement('TR');
+      fillCarRow(tr, car);
+      tr.appendChild(deleteCarCell());
+      return tr;
+    }
+
+    function fillCarRow(tr, car){
+      Object.keys(car).forEach(function(property, index){
+        var td = doc.createElement('TD');
+        if(index)
+          td.textContent = car[property];
+        else
+          td.appendChild(createImage(car[property]));
+        tr.setAttribute('data-js', 'carRow');
+        tr.appendChild(td);
       });
-      row.appendChild(createDeleteButtonCell());
-      docFragment.appendChild(row);
-      $carsTable.appendChild(docFragment);
-    };
+    }
 
-    var createDataCell = function createDataCell(row, item){
-      var data = doc.createElement('TD');
-      if(item.value.match(/png|jpg|gif|svg/gi)){
-          data.appendChild(createIMG(item.value));
-          row.appendChild(data);
-      }else{
-        var text = doc.createTextNode(item.value);
-        data.appendChild(text);
-        row.appendChild(data);
-      }
-    };
-
-    var createDeleteButtonCell = function createDeleteButtonCell(){
+    function deleteCarCell(){
       var td = doc.createElement('TD');
-      var btnDeletar = doc.createElement('BUTTON');
-      btnDeletar.addEventListener('click', deleteLine, false);
-      btnDeletar.textContent = 'X';
-      td.appendChild(btnDeletar);
+      var btn = doc.createElement('BUTTON');
+      btn.textContent = 'X';
+      btn.addEventListener('click', removeRow, false);
+      td.appendChild(btn);
       return td;
-    };
+    }
 
-    var deleteLine = function deleteLine(){
-        $carsTable.removeChild(this.parentElement.parentElement); 
-    };
+    function removeRow(){
+      $tableCars.removeChild(this.parentElement.parentElement);
+    }
 
-    var allFieldsFilled = function allFieldsFilled(){
-      return $inputsForm.every(hasValue);
-    };
+    function createImage(link){
+      var image = doc.createElement('IMG');
+      image.setAttribute('src', link);
+      image.setAttribute('class', 'carImage');
+      return image;
+    }
 
-    var createIMG = function createIMG(src){
-      var img = doc.createElement('IMG');
-      img.setAttribute('src', src);
-      img.setAttribute('class','carImage');
-      return img;
-    };
+    function isAjaxReady(ajax){
+      if(ajax.readyState === 4 && ajax.status === 200)
+        return true;
+      return false;
+    }
 
-    var hasValue = function hasValue(item){
-      return item.value !== "";
-    };
+    function fieldsFilled(){
+      return $inputsForm.every(function(field){
+        return !!field.value;
+      });
+    }
 
-    var setCompanyNamePhone = function setCompanyNamePhone(){
-      if(ajax.readyState === 4 && ajax.status === 200){
-        $companyName.textContent = JSON.parse(ajax.responseText).name;
-        $companyPhone.textContent = JSON.parse(ajax.responseText).phone;
+    function registerNewCar(event){
+      event.preventDefault();
+      if(fieldsFilled())
+        sendCarInfo();
+      else
+        alert('Preencha todos os campos do formulário');
+    }
+
+    function sendCarInfo(){
+      ajaxPostCar.open('POST', 'http://localhost:3000/car');
+      ajaxPostCar.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      var data = generateData();
+      ajaxPostCar.send(data);
+      ajaxPostCar.addEventListener('readystatechange', succesRegister, false);
+    }
+
+    function succesRegister(){
+      if(ajaxPostCar.readyState === 4 && ajaxPostCar.status === 200){
+        cleanTable();
+        ajaxGet();
       }
-    };
+    }
 
-    ajax.open('get', 'http://localhost:8080/company.json');
-    ajax.send();
-    ajax.addEventListener('readystatechange', setCompanyNamePhone);
-    $btnCadastrar.addEventListener('click', registerNewCar);
+    function generateData(){
+      return $inputsForm.reduce(function(acc, item, index){
+        if(index === 1)
+          acc = acc.name + '=' + acc.value + '&' + item.name + '=' + item.value;
+        else
+          acc += '&' + item.name + '=' + item.value;    
+        return acc;
+      },0);
+    }
 
-    return{
-      'registerNewCar' : registerNewCar,
-      'newTableLine' : newTableLine,
-      'createDataCell' : createDataCell,
-      'allFieldsFilled' : allFieldsFilled,
-      'createIMG' : createIMG,
-      'hasValue' : hasValue,
-      'setCompanyNamePhone' : setCompanyNamePhone,
-      'createDeleteButtonCell' : createDeleteButtonCell,
-      'deleteLine' : deleteLine
+    function cleanTable(){
+      var rows = $('[data-js="carRow"]');
+      rows.forEach(function(item){
+        $tableCars.removeChild(item);
+      });
+    }
+
+    function ajaxCompany(){
+      ajaxCompanyInfo.open('GET', 'http://localhost:8080/company.json');
+      ajaxCompanyInfo.send();
+      ajaxCompanyInfo.addEventListener('readystatechange', getCompanyInfo, false);
+    }
+
+    function ajaxGet(){
+      ajaxGetCars.open('GET', 'http://localhost:3000/car');
+      ajaxGetCars.send();
+      ajaxGetCars.addEventListener('readystatechange', getCars, false);
+    }
+    
+    ajaxCompany();
+    ajaxGet();
+    $btnRegister.addEventListener('click', registerNewCar, false);
+
+    return {
+      'getCompanyInfo': getCompanyInfo,
+      'getCars': getCars,
+      'putCarInRow': putCarInRow,
+      'fillCarRow': fillCarRow,
+      'deleteCarCell': deleteCarCell,
+      'removeRow': removeRow,
+      'createImage': createImage,
+      'isAjaxReady': isAjaxReady,
+      'fieldsFilled': fieldsFilled,
+      'registerNewCar': registerNewCar,
+      'sendCarInfo': sendCarInfo,
+      'succesRegister': succesRegister,
+      'generateData': generateData,
+      'cleanTable': cleanTable,
+      'ajaxCompany': ajaxCompany,
+      'ajaxGet': ajaxGet
     };
 
   })();
